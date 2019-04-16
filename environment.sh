477e3784dc8bb59344ec5efd55eb7fc67d38d8c3
matthew-trump <matthew.trump@gmail.com>
2019-04-16 12:04:59 -0700

    with google application credentials

--- environment.sh
+++ environment.sh
@@ -1,1 +1,3 @@
-export PORT=8181
+export PORT=8080
+export GOOGLE_STORAGE_BUCKET_NAME="test-project-95338.appspot.com"
+export GOOGLE_APPLICATION_CREDENTIALS="./test-project-95338-1c15e1b39fc4.json"
